0bc06224f845c5ed1365b69581229bffcd463525
maretskii <maretskii@gmail.com>
2017-09-27 14:28:37 +0300

    Add SolutionWorkspace

--- frontend/src/containers/workspace/solution/SolutionWorkspace.js
+++ frontend/src/containers/workspace/solution/SolutionWorkspace.js
@@ -4,17 +4,16 @@ import PropTypes from 'prop-types';
 import TaskViewer from './TaskViewer';
 
 export default class SolutionWorkspace extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      
+    }
+  }
   render() {
-    const { taskId } = this.props;
     return (
-      <Tabs justified id="workspace-tabs" className="full-height-tabs">
-        <Tab eventKey={1} title="Task">
-          <TaskViewer taskId={taskId} />
-        </Tab>
-        <Tab eventKey={2} title="Solution">
-          <h1>My solution</h1>
-        </Tab>
-      </Tabs>
+      
     );
   }
 }
